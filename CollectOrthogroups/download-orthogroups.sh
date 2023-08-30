dde2600fead5c1043cbc3a3dd5e49306c16758fb
anthony-aylward <aaylward@eng.ucsd.edu>
2023-08-29 22:17:28 -0700

    start on orthogroup collect

---
+++ CollectOrthogroups/download-orthogroups.sh
@@ -0,0 +1,3 @@
+aws s3 cp s3://salk-tm-shared/csat/releases/orthofinder/csat_prots.full_small.orthofinder/Phylogenetic_Hierarchical_Orthogroups.tar.gz Phylogenetic_Hierarchical_Orthogroups.tar.gz
+tar -zxvf Phylogenetic_Hierarchical_Orthogroups.tar.gz Phylogenetic_Hierarchical_Orthogroups/N3.tsv
+rm Phylogenetic_Hierarchical_Orthogroups.tar.gz
