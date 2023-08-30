b0d33229ed3e0564f9f69a8cb0b06bc762b60d6e
anthony-aylward <aaylward@eng.ucsd.edu>
2023-08-29 22:28:18 -0700

    try this

--- CollectOrthogroups/collect_orthogroups.py
+++ CollectOrthogroups/collect_orthogroups.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 ortho = pd.read_table('Phylogenetic_Hierarchical_Orthogroups/N3.tsv',
                       index_col=0, dtype=str)
-ortho['YMv2b'].map(lambda s: s != 'NaN')
+ortho['YMv2b'] = ortho['YMv2b'].map(lambda s: s != 'NaN')
 print(ortho)
