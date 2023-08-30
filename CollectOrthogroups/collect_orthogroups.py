7d9f4ed6b864d79a867b86b21733456e8ea4b250
anthony-aylward <aaylward@eng.ucsd.edu>
2023-08-29 22:32:17 -0700

    try this

--- CollectOrthogroups/collect_orthogroups.py
+++ CollectOrthogroups/collect_orthogroups.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 ortho = pd.read_table('Phylogenetic_Hierarchical_Orthogroups/N3.tsv',
                       index_col=0, dtype=str)
-print(ortho.iloc[:,3:])
+print(ortho.iloc[:,2:])
