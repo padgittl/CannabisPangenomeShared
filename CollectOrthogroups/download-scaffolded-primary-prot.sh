524f2b64d9b128da7c0fd38625b2cc93fe4976f6
anthony-aylward <aaylward@eng.ucsd.edu>
2023-09-27 08:06:30 -0700

    update

--- CollectOrthogroups/download-scaffolded-primary-prot.sh
+++ CollectOrthogroups/download-scaffolded-primary-prot.sh
@@ -1,3 +1,4 @@
+mkdir primary_high_confidence_proteins
 for line in `python scaffolded.py`;
-  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.proteins.fasta.gz ${line}.primary_high_confidence.proteins.fasta.gz;
+  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.proteins.fasta.gz primary_high_confidence_proteins/${line}.primary_high_confidence.proteins.fasta.gz;
 done
