0ce65b6c9761e4501be69b8fa310bf052480dba7
Anthony Aylward <aaylward@salk.edu>
2023-09-27 15:08:07 -0700

    bash strict

--- CollectOrthogroups/download-scaffolded-primary-cds.sh
+++ CollectOrthogroups/download-scaffolded-primary-cds.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 mkdir primary_high_confidence_cds
 for line in `python scaffolded.py`;
   do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.cds.fasta.gz primary_high_confidence_cds/${line}.primary_high_confidence.cds.fasta.gz;
--- CollectOrthogroups/download-scaffolded-primary-prot.sh
+++ CollectOrthogroups/download-scaffolded-primary-prot.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
 mkdir primary_high_confidence_proteins
 for line in `python scaffolded.py`;
   do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.proteins.fasta.gz primary_high_confidence_proteins/${line}.primary_high_confidence.proteins.fasta.gz;
