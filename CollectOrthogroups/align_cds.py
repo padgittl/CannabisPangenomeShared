72532219849a26a95e8f0b4467a881f033ab19c6
anthony-aylward <aaylward@eng.ucsd.edu>
2023-10-13 09:55:11 -0700

    advance align_cds.py

--- CollectOrthogroups/align_cds.py
+++ CollectOrthogroups/align_cds.py
@@ -8,6 +8,9 @@
 
 import shutil
 from argparse import ArgumentParser
+from multiprocessing import Pool
+from functools import partial
+from subprocess import run
 
 # Functions ====================================================================
 
@@ -19,17 +22,24 @@ def check_minimap2():
         raise RuntimeError('minimap2 not found')
 
 
+def splice_align(genome, cds, outdir='.'):
+    run(('minimap2', '-t', '1', ))
+
 
 def parse_arguments():
     parser = ArgumentParser(description='align CDS sequences to scaffolded genomes')
     parser.add_argument('outdir', help='directory for output files')
     parser.add_argument('--genomes', nargs='+', required=True)
     parser.add_argument('--cds', nargs='+', required=True)
+    parser.add_argument('--processes', type=int, help='number of processes')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
+    with Pool(processes=args.processes) as pool:
+        pool.starmap(partial(splice_align, outdir=args.outdir), zip(args.genomes, args.cds))
+
 
 # Execute ======================================================================
 
