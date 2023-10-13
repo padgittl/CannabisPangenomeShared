a9fdb04ba567cd7d48a7e9d5e8992efee7767a7b
anthony-aylward <aaylward@eng.ucsd.edu>
2023-10-13 07:57:36 -0700

    refine

--- CollectOrthogroups/download-scaffolded-primary-cds.sh
+++ CollectOrthogroups/download-scaffolded-primary-cds.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 mkdir primary_high_confidence_cds
-for line in `python scaffolded.py`;
-  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.cds.fasta.gz primary_high_confidence_cds/${line}.primary_high_confidence.cds.fasta.gz;
+for genome in `python scaffolded.py`;
+  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${genome}/${genome}.primary_high_confidence.cds.fasta.gz primary_high_confidence_cds/${genome}.primary_high_confidence.cds.fasta.gz;
 done
--- CollectOrthogroups/download-scaffolded-primary-prot.sh
+++ CollectOrthogroups/download-scaffolded-primary-prot.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 mkdir primary_high_confidence_proteins
-for line in `python scaffolded.py`;
-  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${line}/${line}.primary_high_confidence.proteins.fasta.gz primary_high_confidence_proteins/${line}.primary_high_confidence.proteins.fasta.gz;
+for genome in `python scaffolded.py`;
+  do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${genome}/${genome}.primary_high_confidence.proteins.fasta.gz primary_high_confidence_proteins/${genome}.primary_high_confidence.proteins.fasta.gz;
 done
