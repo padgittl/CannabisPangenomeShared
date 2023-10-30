49783457d39668a25861b8e54cdf9ef836b7ed43
Anthony Aylward <aaylward@salk.edu>
2023-10-29 18:23:29 -0700

    try proteins

--- CollectOrthogroups/merge-prot.sh
+++ CollectOrthogroups/merge-prot.sh
@@ -1,2 +1,2 @@
-mkdir primary_high_confidence_cds_merged
-cat primary_high_confidence_cds/*.fasta.gz | seqkit rmdup -s | bgzip -c > primary_high_confidence_cds_merged/primary_high_confidence.cds.fasta.gz
+mkdir primary_high_confidence_prot_merged
+cat primary_high_confidence_prot/*.fasta.gz | seqkit rmdup -s | bgzip -c > primary_high_confidence_prot_merged/primary_high_confidence.proteins.fasta.gz
