2039d8642416a1f5b5a6bf0afb6e49c5a15beae2
Anthony Aylward <aaylward@salk.edu>
2023-10-29 17:52:06 -0700

    refine

--- CollectOrthogroups/download-scaffolded-primary-prot.sh
+++ CollectOrthogroups/download-scaffolded-primary-prot.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
 mkdir primary_high_confidence_prot
 for genome in `python scaffolded.py`;
   do aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${genome}/${genome}.primary_high_confidence.proteins.fasta.gz primary_high_confidence_prot/${genome}.primary_high_confidence.proteins.fasta.gz;
