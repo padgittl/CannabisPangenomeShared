15ae911ec5a1dde68c2c84a7fee48a70001af8ca
Anthony Aylward <aaylward@salk.edu>
2024-01-21 10:48:29 -0800

    update

--- CollectOrthogroups/filter_aligned_cds.py
+++ CollectOrthogroups/filter_aligned_cds.py
@@ -10,14 +10,16 @@ import os.path
 from argparse import ArgumentParser
 from multiprocessing import Pool
 from functools import partial
+from cigar import Cigar
 
 # Functions ====================================================================
 
 def filter_aligned_cds(paf, outdir='.'):
     with open(paf, 'r') as f_in, open(os.path.join(outdir, os.path.basename(paf)), 'w') as f_out:
         for line in f_in:
-            _, q_len, _, _, _, _, _, _, _, match, _, qual, *_ = line.split()
-            if qual == '60' and int(match) / int(q_len) > 0.8:
+            _, q_len, _, _, _, _, _, _, _, _, _, qual, *_, cig = line.split()
+            match = {op: val for val, op in Cigar(cig).merge_like_ops().items()}.get('M', 0)
+            if qual == '60' and match / int(q_len) > 0.8:
                 f_out.write(line)
 
 
