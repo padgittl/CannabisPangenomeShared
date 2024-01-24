13c6cbfd838239006a4d67e5e7b556917c459416
anthony-aylward <aaylward@eng.ucsd.edu>
2024-01-23 23:59:56 -0800

    try this

--- CollectOrthogroups/map_orthogroups.py
+++ CollectOrthogroups/map_orthogroups.py
@@ -1,8 +1,26 @@
 import pandas as pd
+import os.path
+from scaffolded import SCAFFOLDED
+
+PAF_DIR = 'filtered_cds_cigar'
+
+hogs = tuple(pd.read_table(
+        'orthofinder-proteomes/primary_transcripts/OrthoFinder/Results_Jan21/Phylogenetic_Hierarchical_Orthogroups/N0.tsv',
+        index_col=0)['Cannabis_sativa_high_confidence_prot_merged'].dropna().index)
 
 gene_to_hog = {
     gene: hog for hog, gene_list in pd.read_table(
         'orthofinder-proteomes/primary_transcripts/OrthoFinder/Results_Jan21/Phylogenetic_Hierarchical_Orthogroups/N0.tsv',
         index_col=0)['Cannabis_sativa_high_confidence_prot_merged'].dropna().items()
     for gene in gene_list.split(', ')
-  }
+}
+
+def hap_to_genes(paf_file):
+    return tuple(pd.read_table(paf_file, index_col=0).index)
+
+haps_to_hogs = {hap: {gene_to_hog[g]
+                      for g in hap_to_genes(os.path.join(PAF_DIR, f'{hap}.paf'))
+                      if gene_to_hog.get(g)}
+                 for hap in SCAFFOLDED}
+
+print(pd.DataFrame({hap: [h in haps_to_hogs[hap] for h in hogs] for hap in SCAFFOLDED}, index=hogs))
