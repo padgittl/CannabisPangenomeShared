c804072269419394af728f8aae456d4ca37aad8d
Anthony Aylward <aaylward@salk.edu>
2024-01-31 12:10:24 -0800

    try n1

--- CollectOrthogroups/map_orthogroups.py
+++ CollectOrthogroups/map_orthogroups.py
@@ -3,7 +3,7 @@ import os.path
 from scaffolded import SCAFFOLDED
 
 PAF_DIR = 'filtered_cds_cigar'
-HOG_TSV = 'nolans-proteomes/primary_transcripts/OrthoFinder/Results_Jan30/WorkingDirectory/OrthoFinder/Results_Jan31/Phylogenetic_Hierarchical_Orthogroups/N2.tsv'
+HOG_TSV = 'nolans-proteomes/primary_transcripts/OrthoFinder/Results_Jan30/WorkingDirectory/OrthoFinder/Results_Jan31/Phylogenetic_Hierarchical_Orthogroups/N1.tsv'
 CSAT_COLNAME = 'Cannabis_sativa_high_confidence_prot_merged'
 hogs = tuple(pd.read_table(HOG_TSV, index_col=0)[CSAT_COLNAME].dropna().index)
 gene_to_hog = {
