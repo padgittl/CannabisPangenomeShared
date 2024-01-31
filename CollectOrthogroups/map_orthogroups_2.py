339b78ae95eab8c7ae107fc6fa300b221e0e4ad2
Anthony Aylward <aaylward@salk.edu>
2024-01-31 15:24:24 -0800

    try this

--- CollectOrthogroups/map_orthogroups_2.py
+++ CollectOrthogroups/map_orthogroups_2.py
@@ -6,8 +6,10 @@ PAF_DIR = 'filtered_cds_cigar'
 HOG_TSV = 'nolans-orthofinder/Phylogenetic_Hierarchical_Orthogroups/N30.tsv'
 SINGLETONS_TSV = 'nolans-orthofinder/Orthogroups/Orthogroups_UnassignedGenes.tsv'
 hogs = tuple(pd.read_table(HOG_TSV, index_col=0)[SCAFFOLDED].dropna().index)
-singletons = {gene: og for og, gene_list in pd.read_table(SINGLETONS_TSV, index_col=0)[SCAFFOLDED].dropna().iterrows()
-              for gene in ', '.join(gene_list).split(', ')}
+singletons = {
+    gene: og for og, gene_list in pd.read_table(SINGLETONS_TSV, index_col=0)[SCAFFOLDED].dropna().iterrows()
+    for gene in set(gene_list) if gene != ''
+}
 gene_to_og = {
     gene: hog for hog, gene_list in pd.read_table(HOG_TSV, index_col=0)[SCAFFOLDED].dropna().iterrows()
     for gene in ', '.join(gene_list).split(', ')
