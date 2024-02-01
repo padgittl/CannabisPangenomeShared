5433e7ca6e569869cd4c8ad30170ad16c736953b
Anthony Aylward <aaylward@salk.edu>
2024-01-31 16:37:50 -0800

    try this again

--- CollectOrthogroups/map_orthogroups_2.py
+++ CollectOrthogroups/map_orthogroups_2.py
@@ -19,13 +19,12 @@ gene_to_og = {
 ogs = hogs + tuple(singletons.values())
 gene_to_og.update(singletons)
 
-def hap_to_genes(paf_file, cds_file):
-    return set(tuple(pd.read_table(paf_file, index_col=0).index) + tuple(Fasta(cds_file).keys()))
+def hap_to_genes(paf_file):
+    return set(tuple(pd.read_table(paf_file, index_col=0).index))
 
 haps_to_ogs = {hap: {gene_to_og[g[:-3]]
                         for g in hap_to_genes(
-                            os.path.join(PAF_DIR, f'{hap}.paf'),
-                            os.path.join(CDS_DIR, f'{hap}.primary_high_confidence.cds.fasta')
+                            os.path.join(PAF_DIR, f'{hap}.paf')
                         )
                         if gene_to_og.get(g[:-3])}
                  for hap in SCAFFOLDED}
