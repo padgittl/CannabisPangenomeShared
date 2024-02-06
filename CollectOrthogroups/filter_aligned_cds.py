d185bca6cc88c6ab1d30d6f84461d4d0c189ded1
anthony-aylward <aaylward@eng.ucsd.edu>
2024-02-05 23:40:46 -0800

    update

--- CollectOrthogroups/filter_aligned_cds.py
+++ CollectOrthogroups/filter_aligned_cds.py
@@ -14,13 +14,18 @@ from cigar import Cigar
 
 # Functions ====================================================================
 
-def filter_aligned_cds(paf, outdir='.', use_cigar=False):
+def filter_aligned_cds(
+        paf,
+        outdir: str = '.' ,
+        use_cigar: bool = False,
+        match_percent: int = 80
+    ):
     with open(paf, 'r') as f_in, open(os.path.join(outdir, os.path.basename(paf)), 'w') as f_out:
         for line in f_in:
             _, q_len, _, _, _, _, _, _, _, match, _, qual, *_, cig = line.split()
             if use_cigar:
                 match = {op: val for val, op in Cigar(cig.split(':')[-1]).merge_like_ops().items()}.get('M', 0)
-            if qual == '60' and int(match) / int(q_len) > 0.99:
+            if qual == '60' and int(match) / int(q_len) > match_percent / 100:
                 f_out.write(line)
 
 
@@ -30,13 +35,18 @@ def parse_arguments():
     parser.add_argument('--paf', nargs='+', required=True)
     parser.add_argument('--processes', type=int, help='number of processes')
     parser.add_argument('--cigar', action='store_true', help='use CIGAR to calculate matches')
+    parser.add_argument('--match-percent', type=int, default=80,
+                        help='minimum percent matches for an alignment to pass')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
     with Pool(processes=args.processes) as pool:
-        pool.map(partial(filter_aligned_cds, outdir=args.outdir), args.paf)
+        pool.map(
+            partial(filter_aligned_cds, outdir=args.outdir, cigar=args.cigar, match=args.match),
+            args.paf
+        )
 
 
 # Execute ======================================================================
