ce3700cc20a068032ee09730037152f4ddd78d50
Anthony Aylward <aaylward@salk.edu>
2024-02-05 19:04:47 -0800

    clean up

--- CollectOrthogroups/map_orthogroups_2.py
+++ CollectOrthogroups/map_orthogroups_2.py
@@ -10,18 +10,17 @@ HOG_TSV = 'nolans-orthofinder/Phylogenetic_Hierarchical_Orthogroups/N30.tsv'
 SINGLETONS_TSV = 'nolans-orthofinder/Orthogroups/Orthogroups_UnassignedGenes.tsv'
 LIKELY_CONTAMINANTS = 'csat.likely_contaminants.tsv'
 hogs = tuple(pd.read_table(HOG_TSV, index_col=0)[SCAFFOLDED].dropna(how='all').index)
-singletons = {
-    gene: og for og, gene_list in pd.read_table(SINGLETONS_TSV, index_col=0, dtype=str)[SCAFFOLDED].dropna(how='all').iterrows()
-    for gene in (g for g in gene_list if (not pd.isna(g)))
-}
 gene_to_og = {
     gene: hog for hog, gene_list in pd.read_table(HOG_TSV, index_col=0)[SCAFFOLDED].dropna(how='all').iterrows()
     for gene in ', '.join(g for g in gene_list if (not pd.isna(g))).split(', ')
 }
+# singletons = {
+#     gene: og for og, gene_list in pd.read_table(SINGLETONS_TSV, index_col=0, dtype=str)[SCAFFOLDED].dropna(how='all').iterrows()
+#     for gene in (g for g in gene_list if (not pd.isna(g)))
+# }
 ogs = hogs #+ tuple(singletons.values())
 # gene_to_og.update(singletons)
 contam = set(pd.read_table(LIKELY_CONTAMINANTS)['GID'])
-
 
 def hap_to_genes(paf_file, cds_file):
     with open(paf_file, 'r') as f:
