564dd19aad62c7a4cb58307339e2e4535973903c
Anthony Aylward <aaylward@salk.edu>
2024-02-07 13:59:38 -0800

    include publics

--- CollectOrthogroups/download_genomes.sh
+++ CollectOrthogroups/download_genomes.sh
@@ -3,3 +3,5 @@ aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/ genomes/ \
   --recursive --exclude "*" --include "*.softmasked.fasta.gz"
 aws s3 cp s3://salk-tm-shared/csat/releases/not_scaffolded/ genomes/ \
   --recursive --exclude "*" --include "*.softmasked.fasta.gz"
+aws s3 cp s3://salk-tm-shared/csat/releases/publics/ genomes/ \
+  --recursive --exclude "*" --include "*.softmasked.fasta.gz"
--- CollectOrthogroups/download_primary_cds.sh
+++ CollectOrthogroups/download_primary_cds.sh
@@ -3,4 +3,6 @@ aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/ primary_high_confidence/
   --recursive --exclude "*" --include "*.primary_high_confidence.cds.fasta.gz" --include "*.primary_high_confidence.bed.gz"
 aws s3 cp s3://salk-tm-shared/csat/releases/not_scaffolded/ primary_high_confidence/ \
   --recursive --exclude "*" --include "*.primary_high_confidence.cds.fasta.gz" --include "*.primary_high_confidence.bed.gz"
+aws s3 cp s3://salk-tm-shared/csat/releases/publics/ primary_high_confidence/ \
+  --recursive --exclude "*" --include "*.primary_high_confidence.cds.fasta.gz" --include "*.primary_high_confidence.bed.gz"
 gunzip primary_high_confidence/*/*.primary_high_confidence.cds.fasta.gz
