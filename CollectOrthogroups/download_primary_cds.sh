73df1a0a80d633aac1e4b1b00396ac6efa1c5d62
Anthony Aylward <aaylward@salk.edu>
2024-02-07 13:31:33 -0800

    debug

--- CollectOrthogroups/download_primary_cds.sh
+++ CollectOrthogroups/download_primary_cds.sh
@@ -3,4 +3,4 @@ aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/ primary_high_confidence/
   --recursive --exclude "*" --include "*.primary_high_confidence.cds.fasta.gz" --include "*.primary_high_confidence.bed.gz"
 aws s3 cp s3://salk-tm-shared/csat/releases/not_scaffolded/ primary_high_confidence/ \
   --recursive --exclude "*" --include "*.primary_high_confidence.cds.fasta.gz" --include "*.primary_high_confidence.bed.gz"
-gunzip primary_high_confidence_scaffolded/*.primary_high_confidence.cds.fasta.gz
+gunzip primary_high_confidence/*/*.primary_high_confidence.cds.fasta.gz
