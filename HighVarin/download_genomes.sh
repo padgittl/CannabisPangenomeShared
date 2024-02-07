27384d4b0bc745f7c78027795d4720a7cde2faf8
Anthony Aylward <aaylward@salk.edu>
2024-02-07 15:57:05 -0800

    update varin genomes

--- HighVarin/download_genomes.sh
+++ HighVarin/download_genomes.sh
@@ -1,8 +1,11 @@
 mkdir varin_genomes non_varin_genomes
-for varin in COFBa COSVb DPFBb DPSVb MBFBb MBSVb OFBb PPFBb SDFBb SKFBb SVA12a SVA6a TKFBb TWFBb TWSVb UFBb WCFBb; do
+for varin in EH23a EH23b COFBa DPFBb MBFBb OFBb PPFBb SDFBb SKFBb TKFBb TWFBb UFBb WCFBb SVA12a SVA6a TWSVa COSVb DPSVb MBSVb; do
   aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${varin}/${varin}.softmasked.fasta.gz varin_genomes/${varin}.softmasked.fasta.gz
 done
+for varin in HO40 FB191 USV WCSV SKSV ACBD SHH24 SSV SV2 SSHS WH106; do
+  aws s3 cp s3://salk-tm-shared/csat/releases/not_scaffolded/${varin}/${varin}.softmasked.fasta.gz varin_genomes/${varin}.softmasked.fasta.gz
+done
 
-for non_varin in COFBb COSVa DPFBa DPSVa MBFBa MBSVa OFBa PPFBa SDFBa SKFBa SVA12b SVA6b TKFBa TWFBa TWSVa UFBa WCFBa; do
+for non_varin in COFBb DPFBa MBFBa OFBa PPFBa SDFBa SKFBa TKFBa TWFBa UFBa WCFBa; do
   aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${non_varin}/${non_varin}.softmasked.fasta.gz non_varin_genomes/${non_varin}.softmasked.fasta.gz
 done
