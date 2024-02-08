77903ee52f4bc6f4fd42934ace98bd00c92cd80b
anthony-aylward <aaylward@eng.ucsd.edu>
2024-02-08 10:46:08 -0800

    refine

--- HighVarin/download_genomes.sh
+++ HighVarin/download_genomes.sh
@@ -5,7 +5,7 @@ done
 for varin in HO40 FB191 USV WCSV SKSV ACBD SHH24 SSV SV2 SSHS WH106; do
   aws s3 cp s3://salk-tm-shared/csat/releases/not_scaffolded/${varin}/${varin}.softmasked.fasta.gz varin_genomes/${varin}.softmasked.fasta.gz
 done
-
 for non_varin in COFBb DPFBa MBFBa OFBa PPFBa SDFBa SKFBa TKFBa TWFBa UFBa WCFBa; do
   aws s3 cp s3://salk-tm-shared/csat/releases/scaffolded/${non_varin}/${non_varin}.softmasked.fasta.gz non_varin_genomes/${non_varin}.softmasked.fasta.gz
 done
+gunzip varin_genomes/*
