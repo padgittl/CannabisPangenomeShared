0a71426eeaecac1e0a605242dcc4a08e4709cbd7
Anthony Aylward <aaylward@salk.edu>
2024-02-10 14:36:50 -0800

    scaffolded.py

---
+++ CollectKmers/scaffolded.py
@@ -0,0 +1,15 @@
+SCAFFOLDED = [
+    'AH3Ma', 'AH3Mb', 'BCMa', 'BCMb', 'BOAXa', 'BOAXb', 'COFBa', 'COFBb',
+    'COSVa', 'COSVb', 'DPFBa', 'DPFBb', 'DPSVa', 'DPSVb', 'EH23a', 'EH23b',
+    'FCS1a', 'FCS1b', 'GERv1a', 'GERv1b', 'GRMa', 'GRMb', 'H3S1a', 'H3S1b',
+    'H3S7a', 'H3S7b', 'KCDv1a', 'KCDv1b', 'KOMPa', 'KOMPb', 'MBFBa', 'MBFBb',
+    'MBSVa', 'MBSVb', 'MM3v1a', 'MM3v1b', 'NLv1a', 'NLv1b', 'OFBa', 'OFBb',
+    'PPFBa', 'PPFBb', 'S8Ha', 'S8Hb', 'SAN2a', 'SAN2b', 'SDFBa', 'SDFBb',
+    'SHH5a', 'SHH5b', 'SKFBa', 'SKFBb', 'SN1v3a', 'SN1v3b', 'SODLa', 'SODLb',
+    'STHa', 'STHb', 'SVA12a', 'SVA12b', 'SVA6a', 'SVA6b', 'SZFBa', 'SZFBb',
+    'TKFBa', 'TKFBb', 'TWFBa', 'TWFBb', 'TWSVa', 'TWSVb', 'UFBa', 'UFBb',
+    'WCFBa', 'WCFBb', 'WHWa', 'WHWb', 'YMv2a', 'YMv2b'
+]
+
+if __name__ == '__main__':
+    print(' '.join(SCAFFOLDED))
